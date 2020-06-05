f5fb68e84b0420ba576e96706a2b4a4cf98b374c
Mark Wolff <mark.wolff@microsoft.com>
2020-06-05 15:28:52 -0400

    bump dependencies

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import {act} from 'react-dom/test-utils';
 import App from './App';
 
-jest.mock('@microsoft/applicationinsights-react-js', () => ({
-  withAITracking: (reactPlugin, Component) => (Component),
-}));
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
   act(() => {
