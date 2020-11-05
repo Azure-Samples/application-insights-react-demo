8fc1ecb6e8d94eed2dc412e1aa892dc61cba2185
Sly Gryphon <sly@gamertheory.net>
2020-11-05 11:45:48 +1000

    Use proxy from server to existing client app

--- server/Demo.Api/Startup.cs
+++ server/Demo.Api/Startup.cs
@@ -63,7 +63,7 @@ namespace Demo.Api
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseReactDevelopmentServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");
                 }
             });
         }
