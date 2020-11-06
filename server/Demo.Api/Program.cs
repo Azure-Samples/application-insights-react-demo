859a93cf1d72e493440f3cb5dd243e195a91f6e4
Sly Gryphon <sly@gamertheory.net>
2020-11-06 14:46:51 +1000

    Set default server format to W3C

--- server/Demo.Api/Program.cs
+++ server/Demo.Api/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -13,6 +14,7 @@ namespace Demo.Api
     {
         public static void Main(string[] args)
         {
+            Activity.DefaultIdFormat = ActivityIdFormat.W3C;
             CreateHostBuilder(args).Build().Run();
         }
 
